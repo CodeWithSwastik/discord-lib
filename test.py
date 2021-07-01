e9494867a85f1f347d51c3bcd5da0809468a9bc6
unknown <codewithswastik@gmail.com>
2021-07-01 13:09:29 +0530

    Fix test.py

---
+++ test.py
@@ -0,0 +1,7 @@
+import json
+from discordlib.client import Client
+
+with open("tests/bot.json", "r") as f:
+    bot = Client(json.load(f))
+    
+bot.run()
--- tests/test.py
+++
@@ -1,9 +0,0 @@
-import json
-from discordlib.client import build_bot_from_config
-
-with open("tests/bot.json", "r") as f:
-    e = json.load(f)
-    config = e.get("config")
-    bot = build_bot_from_config(config)
-    
-bot.run(config["token"])
